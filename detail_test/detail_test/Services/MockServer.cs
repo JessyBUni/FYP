294fc50bd2d2ce727dc68e190a724653d0670513
Jessica <J.Buckley1@2015.ljmu.ac.uk>
2019-02-05 21:47:50 +0000

    Initial Commit of Files

---
+++ detail_test/detail_test/Services/MockServer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace detail_test.Services
+
+{
+    public class MockServer
+    {
+        List<User> registeredUsers;
+        public MockServer()
+        {
+            registeredUsers = new List<User>();
+
+            User person = new User("test@gmail.com", "test", false);
+            registeredUsers.Add(person);
+            person = new User("test", "1", false);
+            registeredUsers.Add(person);
+        }
+        public void updateUserSubscription(string user,int sub)
+        {
+            User field = new User();
+            field = registeredUsers.Find(x => x.Username.Equals(user));
+            field.Subscription = sub;
+        }
+        public void updateUserProgress(string user, int prog)
+        {
+            User field = new User();
+            field = registeredUsers.Find(x => x.Username.Equals(user));
+            field.progress = prog;
+        }
+        public bool checkServer(string user, string pass, bool tok, out int sub, out int prog )
+        {
+            User field = new User();
+            field=registeredUsers.Find(x => x.Username.Equals(user));
+            try
+            {
+                if (field.password==pass)
+                {
+                    sub = field.Subscription;
+                    prog = field.progress;
+                    return true;
+                }
+                else
+                {
+                    sub = 0;
+                    prog = 0;
+                    return false;
+                }
+            }
+            catch
+            {
+                sub = 0;
+                prog = 0;
+                return false;
+            }
+
+        }
+    }
+}
---
+++ detail_test/detail_test/ViewModels/LoginViewModel.cs
@@ -0,0 +1,67 @@
+﻿
+using System;  
+using System.ComponentModel;  
+using System.Windows.Input;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using detail_test.Services;
+
+namespace detail_test.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public Action DisplayInvalidLoginPrompt;
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+        public ICommand SubmitCommand { protected set; get; }
+        public bool SubmitInfo (out int subLevel, out int progress)
+        { 
+            if (email==null || password == null)
+            {
+                /* turn on for deploy
+                DisplayInvalidLoginPrompt();
+                subLevel = 0;
+                progress = 0;              
+                return false;*/
+                subLevel = 1;
+                progress = 3;
+                return true; // turn off for deploy
+            }
+            MockServer mockServer = new MockServer();
+            bool b= mockServer.checkServer(email,password,true, out int sub, out int prog);
+            if (b)
+            {
+                subLevel = sub;
+                progress = prog;
+                return true;
+            }
+            else
+            {
+                subLevel = 0;
+                progress = 0;
+                return false;
+            }
+
+
+        }
+    }
+}
